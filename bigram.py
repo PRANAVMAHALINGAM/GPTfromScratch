cf18ff1d10fc07f507005d5591c2f7e427e86914
PRANAVMAHALINGAM <mpranavmahalingam@gmail.com>
2026-06-02 00:03:16 -0400

    day3 update

--- bigram.py
+++ bigram.py
@@ -6,9 +6,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embed=32
@@ -59,6 +59,63 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    def __init__(self,head_size):
+        super().__init__()
+        self.key=nn.Linear(n_embed,head_size,bias=False)
+        self.query=nn.Linear(n_embed,head_size,bias=False)
+        self.value=nn.Linear(n_embed,head_size,bias=False)
+        self.register_buffer('tril',torch.tril(torch.ones(block_size,block_size)))
+
+    def forward(self,x):
+        B,T,C=x.shape
+        k=self.key(x)
+        q=self.query(x)
+
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei=wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        #perform weighted aggregation of the values
+        v=self.value(x)
+        out=wei @ v # (B, T, T) @ (B,T, C)->(B,T,C)
+        return out
+class MultiHeadAttention(nn.Module):
+    def __init__(self,num_heads,head_size):
+        super().__init__()
+        self.heads=nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        # self.proj=nn.Linear(n_embed,n_embed)
+
+    def forward(self,x):
+        return torch.cat([h(x) for h in self.heads],dim=-1)
+        
+class FeedForward(nn.Module):
+    def __init__(self,n_embed):
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Linear(n_embed,n_embed),
+            nn.ReLU(),     
+        )
+
+    def forward(self,x):
+        return self.net(x)       
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+    def __init__(self,n_embed,n_head):
+        #n_embed: embedding dimension
+        #n_head: number of attention heads we like
+        super().__init__()
+        headsize=n_embed//n_head
+        self.sa=MultiHeadAttention(n_head,headsize) 
+        self.ffwd= FeedForward(n_embed)
+        
+    
+    def forward(self,x):
+        x=self.sa(x)
+        x=self.ffwd(x)
+        return x
+
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -67,6 +124,13 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table =nn.Embedding(block_size,n_embed)
+        self.blocks=nn.Sequential(
+            Block(n_embed,n_head=4),
+            Block(n_embed,n_head=4),
+            Block(n_embed,n_head=4),
+        )
+        # self.sa_heads=MultiHeadAttention(4,n_embed//4) #4 heads and each head size is n_embed//4=32/4=8
+        # self.ffwd=FeedForward(n_embed) # feed forward network
         self.lm_head=nn.Linear(n_embed,vocab_size)
 
     def forward(self, idx, targets=None):
@@ -76,6 +140,9 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,n_embed)
         pos_emb = self.position_embedding_table(torch.arange(T,device=device))# (T,C)
         x=tok_emb+pos_emb # (B,T,C)
+        x=self.blocks(x)
+        # x= self.sa_heads(x) #apply one self attention head (B,T,C)
+        # x= self.ffwd(x) #apply feed forward (B,T,C)
         logits=self.lm_head(x)# (B,T,vocab_size)
 
         if targets is None:
@@ -89,10 +156,12 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
-        # idx is (B, T) array of indices in the current context
+        # idx is (B, T) array of indices in the current context (may increase after each iteration) line 129 due to concatennation
         for _ in range(max_new_tokens):
+            #crop idx to the  last block_size tokens
+            idx_cond= idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
